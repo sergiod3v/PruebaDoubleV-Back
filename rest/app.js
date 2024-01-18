855015b3962685c18582a74d3debc13be5f1761c
scamachoc1 <sergio@xploy.net>
2024-01-18 16:47:39 -0500

    postgres connection

--- rest/app.js
+++ rest/app.js
@@ -1,5 +1,3 @@
-// app.js
-
 const express = require("express");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
--- rest/db/db.js
+++ rest/db/db.js
@@ -1,12 +1,14 @@
 // db/db.js
 
 const { Pool } = require("pg");
+const fs = require("fs");
+const path = require("path");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: "root",
+  host: "root",
   database: "tickets",
-  password: "password",
+  password: "root",
   port: 5432,
 });
 
